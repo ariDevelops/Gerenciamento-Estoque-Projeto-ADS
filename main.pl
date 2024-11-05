4b30c18d785c3bb06ff93f81e2155ce6ec0bef36
Ariel Vieira <ariel.vieira@yandex.com>
2024-11-05 14:46:20 -0300

    tela de cadastro

--- main.pl
+++ main.pl
@@ -1,4 +1,6 @@
 #!/usr/bin/perl
+#Desenvolvimento do Sistema de Gerenciamento de Estoque - Ariel Gomes Vieira 
+#04/11/2024
 use warnings;
 use strict;
 use autodie;
@@ -16,8 +18,12 @@ binmode(STDOUT, ":utf8");
 my $mw = MainWindow->new;
 $mw->geometry('800x500');
 $mw->configure(-background => 'grey',
-               -title => 'estoque');
+               -title => 'Easy Estoque');
 
+
+
+
+#---------------------------------------------------------------------
 # cabeçalho e Barra de ferramentas
 my $header = $mw->Frame(-background => "blue",
                         -relief => "groove",
@@ -39,7 +45,7 @@ $header->Photo(-file => './assets/32px/lixo1.png')
 
 # botões ferramentas
 
-#----------- Entrada de produtos ---------------
+# Entrada de produtos 
 my $entProduto = $header->Checkbutton(
     -image => $entrada,
     -indicatoron => 0,
@@ -49,7 +55,8 @@ my $entProduto = $header->Checkbutton(
     -highlightbackground => 'blue',
     -relief => 'flat',
     -borderwidth => 0,
-    -cursor => 'hand2'
+    -cursor => 'hand2',
+    -command => \&cadastrar
 );
 $entProduto->configure(-width => '40', 
                        -height => '40');
@@ -59,7 +66,7 @@ $entProduto->pack(-side => 'left',
 
 
 
-#----------- Banco de dados ---------------
+# Banco de dados
 my $dbProdutos = $header->Checkbutton(
     -image => $db,
     -indicatoron => 0,
@@ -79,7 +86,7 @@ $dbProdutos->pack(-side => 'left',
 
 
 
-#----------- Envio de Produtos ---------------
+# Envio de Produtos 
 my $envProdutos = $header->Checkbutton(
     -image => $envios,
     -indicatoron => 0,
@@ -99,7 +106,7 @@ $envProdutos->pack(-side => 'left',
                   -pady => 5);
 
 
-#----------- Faturamento ---------------
+# Faturamento 
 my $faturamento = $header->Checkbutton(
     -image => $vendas,
     -indicatoron => 0,
@@ -118,7 +125,7 @@ $faturamento->pack(-side => 'left',
                   -padx => 5, 
                   -pady => 5);
 
-#----------- Perfil do Estoquista ---------------
+# Perfil do Estoquista 
 my $estoquistaPerfil = $header->Checkbutton(
     -image => $estoquista,
     -indicatoron => 0,
@@ -137,7 +144,7 @@ $estoquistaPerfil->pack(-side => 'left',
                   -padx => 5, 
                   -pady => 5);
 
-#----------- Lixeira ---------------
+# Lixeira 
 my $lixo = $header->Checkbutton(
     -image => $lixeira,
     -indicatoron => 0,
@@ -150,15 +157,11 @@ my $lixo = $header->Checkbutton(
     -cursor => 'hand2',
 );
 $lixo->configure(-width => '40', 
-                       -height => '40');
+                 -height => '40');
 
 $lixo->pack(-side => 'right',
                   -padx => 5, 
                   -pady => 5);
-
-
-
-
 
 
 
@@ -185,4 +188,161 @@ $footer->Label(
     ->pack(-side => 'left');
 
 
+
+#---------------------------------------------------------------------
+# Area de Texto 
+my $text = $mw->Scrolled("Listbox");
+$text->configure(-background => 'white', #'LightBlue',
+                 -foreground => 'darkblue',
+                 -relief => 'ridge',
+                 -borderwidth => 5,
+                 -cursor => 'pencil',
+                 -width => 60,
+                 -height => 20,
+                 #-font => 'default',
+                 #-highlightcolor => color,
+                 #-highlightbackground => color,
+                 #-insertbackground => color,
+                 #-insertborderwidth => amount,
+                 -selectbackground => 'darkblue',
+                 #-selectborderwidth => amount,
+                 -selectforeground => 'white',
+                 );
+$text->pack(-side => 'left',
+            -anchor => 'n',
+            #-fill => 'y'
+);
+$text->insert('end', "produto / 650563");
+
+
+
+#---------------------------------------------------------------------
+#Pesquisa de codigos
+
+#botao
+$mw->Button(-text => 'Busca',
+            -cursor => 'hand2',
+#            -command => sub { $text->destroy if Tk::Exists($text); }
+             -command => \&buscarDados 
+            )->pack( -side => 'left',
+                     -anchor => 'n',
+                                     );
+
+#input
+my $busca = $mw->Entry(-background => 'white',
+                       -text => 'codigo Produto',
+                       -width => 25);
+$busca->pack(-side => 'left',
+             -anchor => 'n',
+             -padx => 3,
+             -ipady => 3
+           );
+
+#informação
+my $info = 'nulo';
+$mw->Label(-textvariable => \$info,
+           -relief => 'flat')->pack(-side => 'left',
+                                    -anchor => 'n',
+                                    -pady => 5,
+                                    -padx => 7);
+
 MainLoop;
+
+
+
+
+########################################################################################
+#                                   Algoritmos Logicos
+########################################################################################
+
+
+
+#-------------------------Algoritmo de Busca ------------------------------
+sub buscarDados {
+    $info = 'Buscando ...';
+    $text->delete("1.0", "end");
+        if(!open(FH, "db.txt")) {
+            $text->insert("end", "Error: Base de dados não encontrada\n");
+            return;
+        }
+        while( <FH> ) { $text->insert("end", $_); }
+        close(FH);
+        $info = "Carregado";
+
+}
+
+
+#-------------------------Janela de Cadastro------------------------------
+sub cadastrar {
+    my $winCadastro = $mw->Toplevel();
+    $winCadastro->geometry('400x356');
+    $winCadastro->configure(-background => 'white',
+                            -title => "Cadastro de produto");
+
+    #Codigo
+    $winCadastro->Label(-text => 'codigo DANFE:',
+                        -relief => 'flat',
+                        -background => 'white')->pack(-side => 'top',
+                                                      -anchor => 'w',
+                                                      -pady => 5);
+
+    my $numero = $winCadastro->Entry(-background => 'blue',
+                                     -foreground => 'white',
+                                     -font => 'default');
+    $numero->pack(-side => 'top');
+
+
+    #Nome do produto
+    $winCadastro->Label(-text => 'Nome Produto:',
+                        -relief => 'flat',
+                        -background => 'white')->pack(-side => 'top',
+                                                      -anchor => 'w',
+                                                      -pady => 5);
+
+    my $nome = $winCadastro->Entry(-background => 'blue',
+                                     -foreground => 'white',
+                                     -font => 'default');
+    $nome->pack(-side => 'top');
+
+    # Data
+    $winCadastro->Label(-text => 'Data de chegada:',
+                        -relief => 'flat',
+                        -background => 'white')->pack(-side => 'top',
+                                                      -anchor => 'w',
+                                                      -pady => 5);
+
+    my $data = $winCadastro->Entry(-background => 'blue',
+                                     -foreground => 'white',
+                                     -font => 'default');
+    $data->pack(-side => 'top');
+
+
+    # Departamento Categoria
+    $winCadastro->Label(-text => 'Setor ("categoria"):',
+                        -relief => 'flat',
+                        -background => 'white')->pack(-side => 'top',
+                                                      -anchor => 'w',
+                                                      -pady => 5);
+
+    my $setor = $winCadastro->Entry(-background => 'blue',
+                                     -foreground => 'white',
+                                     -font => 'default');
+    $setor->pack(-side => 'top');
+
+
+
+
+     my $cadastrar = $winCadastro->Button(-text => 'Cadastrar', 
+                                          -cursor => 'hand2',
+                                          -activebackground => 'grey',
+                                          -command => 
+    # Evento de Cadastro
+    sub { exit }
+
+
+     );
+     $cadastrar->pack( -side => 'bottom',
+                       -fill => 'x');
+}
+
+#sub inserirItem {}
